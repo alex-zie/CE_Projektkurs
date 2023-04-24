15d130bc0bacb0e2e0631b6c85d6c696bd083c01
Erik Ebert <erik.byerico@gmail.com>
2023-04-24 14:32:19 +0200

    Updated Comments

--- FEM.py
+++ FEM.py
@@ -70,11 +70,11 @@ nodes = np.array(nodes).astype(float)
 bars = np.array(bars)
 
 # Äußere Kräfte
-F = np.zeros_like(nodes)
-F[16, 0] = 1
-F[17, 0] = 1
-F[18, 0] = 1
-F[19, 0] = 1
+P = np.zeros_like(nodes)
+P[16, 0] = 1
+P[17, 0] = 1
+P[18, 0] = 1
+P[19, 0] = 1
 
 # Lager Verschiebung
 Ur = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # 4 Festlager = 4*3 blockierte Freiheitsgrade
@@ -98,24 +98,28 @@ def TrussAnalysis():
     # Geometrie
     d = nodes[bars[:, 1], :] - nodes[bars[:, 0], :]  # Endknoten - Anfangsknoten
     L = np.sqrt((d ** 2).sum(axis=1))  # Länge der Balken (Euklidische Norm)
-    orientations = d.T / L  # Richtungsvektoren der Balken
+    orientations = d.T / L  # Richtungsvektoren der Balken (Transponieren für Dimensionen)
     trans = np.concatenate((-orientations.T, orientations.T), axis=1)  # Transformationsvektor lokal -> global
 
     # Steifigkeitsmatrix
     K = np.zeros([NDOF, NDOF])
     for k in range(NE):
         aux = DOF * bars[k, :]
+        # Indices der vom Element betroffenen Knoten für Position der Summierung
         index = np.r_[aux[0]:aux[0] + DOF, aux[1]:aux[1] + DOF]
-        ES = np.dot(trans[k][np.newaxis].T * E * A, trans[k][np.newaxis]) / L[k]  # lokale Steifigkeiten
+        # lokale Steifigkeiten, np.newaxis für 0 Zeile(4x4)
+        ES = np.dot(trans[k][np.newaxis].T * E * A, trans[k][np.newaxis]) / L[k]
+        # Globale Steifigkeiten durch Summierung der Einzelsteifigkeiten, Position !
         K[np.ix_(index, index)] = K[np.ix_(index, index)] + ES
 
-    freeDOF = DOFCON.flatten().nonzero()[0]
-    supportDOF = (DOFCON.flatten() == 0).nonzero()[0]
-    Kff = K[np.ix_(freeDOF, freeDOF)]
-    Kfr = K[np.ix_(freeDOF, supportDOF)]
+    freeDOF = DOFCON.flatten().nonzero()[0]  # Prüfe, welche Knoten FG > 0 haben
+    supportDOF = (DOFCON.flatten() == 0).nonzero()[0]  # Knoten mit Lagern
+    Kff = K[np.ix_(freeDOF, freeDOF)]  # Vollkommen Bewegliche knoten
+    Kfr = K[np.ix_(freeDOF, supportDOF)]  # Teilweise bewegliche Knoten
     Krf = Kfr.T
     Krr = K[np.ix_(supportDOF, supportDOF)]  # für die Lagerkräfte
-    Pf = F.flatten()[freeDOF]
+    # Kraftmatrix passend zu K mit nicht null Einträgen, wie oben definiert
+    Pf = P.flatten()[freeDOF]
     # Uf = np.linalg.solve(Kff,Pf)
     Uf = np.linalg.lstsq(Kff, Pf)[
         0]  # Deformation an jedem Freiheitsgrad # least squares damit auch überbestimmte Systeme fkt.
@@ -123,8 +127,9 @@ def TrussAnalysis():
     U[freeDOF] = Uf
     U[supportDOF] = Ur
     U = U.reshape(NN, DOF)
+    # Verschiebungsvektor für die einzelnen Elemente
     u = np.concatenate((U[bars[:, 0]], U[bars[:, 1]]), axis=1)
-    N = E * A / L[:] * (trans[:] * u[:]).sum(axis=1)  # externe Kräfte
+    N = E * A / L[:] * (trans[:] * u[:]).sum(axis=1)  # interne Kräfte
     R = (Krf[:] * Uf).sum(axis=1) + (Krr[:] * Ur).sum(axis=1)  # Reaktionskräfte
     R = R.reshape(4, DOF)  # 4 ist die Anzahl der Lager
     return np.array(N), np.array(R), U
@@ -135,9 +140,11 @@ def Plot(nodes, c, lt, lw, lg):
     plt.gca().set_aspect('auto')
     # plt.gca(projection='3d')
     for i in range(len(bars)):
+        # Jeweilige Start und Endkoordiante
         xi, xf = nodes[bars[i, 0], 0], nodes[bars[i, 1], 0]
         yi, yf = nodes[bars[i, 0], 1], nodes[bars[i, 1], 1]
         zi, zf = nodes[bars[i, 0], 2], nodes[bars[i, 1], 2]
+        # Plotte die Elemente
         line, = plt.plot([xi, xf], [yi, yf], [zi, zf], color=c, linestyle=lt, linewidth=lw)
     line.set_label(lg)
     plt.legend(prop={'size': 7})
@@ -146,6 +153,7 @@ def Plot(nodes, c, lt, lw, lg):
 # %% Result
 N, R, U = TrussAnalysis()
 print('Axial Forces (positive = tension, negative = compression)')
+# Anschaulichkeit
 print(N[np.newaxis].T)
 print('Reaction Forces (positive = upward, negative = downward')
 print(R)
@@ -153,6 +161,7 @@ print('Deformation at nodes')
 print(U)
 Plot(nodes, 'gray', '--', 1, 'Undeformed')
 scale = 5
+# Berechne die neue Position der Knoten
 Dnodes = U * scale + nodes
 Plot(Dnodes, 'red', '-', 2, 'Deformed')
 plt.show()
