23fb03b8beda9eca60a99ff5eaa38e1b68134a13
Alex <alexander.zieschang@gmail.com>
2023-06-23 11:05:56 +0200

    brought back by dear colors 🥲

--- fem.py
+++ fem.py
@@ -157,22 +157,21 @@ class FEM:
 
         return K
 
-    def computeWeight(self): #TODO comment @Alex
+    def computeWeight(self):
+        """
+        Returns a force vector that assigns a force to each node by averaging the weight of the connected bars
+        """
         bars = self.truss.bars
         nOutgoingBars = np.zeros_like(self.truss.nodes[:, 0])
-
         # compute for each node (index) the number of outgoing bars
         for bar in bars:
             nOutgoingBars[bar] = nOutgoingBars[bar] + 1
-
         masses = self.truss.mass
         weights = np.zeros_like(self.truss.nodes[:, 0])
         # add a weight to the node equalling the mass of a connected bar
         for i in range(len(bars)):
             weights[bars[i]] = weights[bars[i]] + masses[i]
-
         return 9.81 * weights / nOutgoingBars
-
     def addWind(self, speed, axis, dir):
         """
         speed in m/s
@@ -183,61 +182,98 @@ class FEM:
             raise Exception("dir has to be either -1 or 1")
         if not (axis == 0 or axis == 1):
             raise Exception("axis can only take values 0 (x), 1 (y)")
-
+        
+        A = self.truss.A*np.ones(self.num_bars)
+        
         self.wind = True
-
         message = "Simulating wind with " + str(speed) + " m/s (" + str(round(speed * 3.6, 2)) + " km/h) blowing in "
         message += "negative " if dir == -1 else "positive "
         message += "x" if axis == 0 else "y"
         message += "-direction."
         print(message)
-
         if axis == 0:
             if dir == -1:
                 bars = self.truss.bars[self.truss.x_positive_side]
                 lengths = self.truss.lengths[self.truss.x_positive_side]
+                A = A[self.truss.x_positive_side]
                 self.wind_dir = 0
             else:
                 bars = self.truss.bars[self.truss.x_negative_side]
                 lengths = self.truss.lengths[self.truss.x_negative_side]
+                A = A[self.truss.x_negative_side]
                 self.wind_dir = 1
         else:
             if dir == -1:
                 bars = self.truss.bars[self.truss.y_positive_side]
                 lengths = self.truss.lengths[self.truss.y_positive_side]
+                A = A[self.truss.y_positive_side]
                 self.wind_dir = 2
             else:
                 bars = self.truss.bars[self.truss.y_negative_side]
                 lengths = self.truss.lengths[self.truss.y_negative_side]
+                A = A[self.truss.y_negative_side]
                 self.wind_dir = 3
-
         nOutgoingBars = np.zeros_like(self.truss.nodes[:, 0])
         # compute for each node (index) the number of outgoing bars
         for bar in bars:
             nOutgoingBars[bar] = nOutgoingBars[bar] + 1
-
         # height of the bars
-        area_bars = self.truss.A ** 0.5 * lengths
-
+        area_bars = np.multiply(A ** 0.5, lengths)
         # add wind force to node for each bar that is connected to it
         # wind pressure: 0.5 * rho * v^2 [m/s] * A [m^2]
         forces = np.zeros_like(self.truss.nodes[:, 0])
         for i in range(len(bars)):
             forces[bars[i]] = forces[bars[i]] + 0.5 * area_bars[
                 i] * 1.2 * speed ** 2  # 1.2 is the air density at sea level
-
         w_forces = np.zeros_like(self.truss.F)
         nOutgoingBars[nOutgoingBars == 0] = 1  # prevent division by zero
         w_forces[:, axis] = dir * forces / nOutgoingBars
         self.truss.addExternalForces(w_forces)
         self.TrussAnalysis()
-
     def getTension(self):
-        """
-        Returns the tension of each element.
-        """
         return self.N / self.truss.A
-
+    def optimize_crossections(self, max_A, crit_tension):
+        """
+        Minimizes the crossections in the interval [A, max_A] based on a critical tension
+        returns: Array of crossections
+        """
+        abs_max_tension = np.max([np.abs(np.min(self.getTension())), np.abs(np.max(self.getTension()))])
+        if isinstance(self.truss.A, float) or isinstance(self.truss.A, int):
+            min_A = self.truss.A
+            self.truss.A = np.zeros(len(self.truss.bars))
+        else:
+            min_A = np.max(self.truss.A)
+        while abs_max_tension > crit_tension:
+            critical_bars = np.abs(self.getTension()) > 0.999*crit_tension # indices where tension is exceeded # 0.995 to make it slightly smaller so that increased weight force is accounted for
+            self.truss.A[critical_bars] = np.abs(self.N[critical_bars]) / (0.999*crit_tension) # increase crossections here
+            self.truss.A[self.truss.A < min_A] = min_A # keep crossections whithin allowed range
+            self.truss.A[self.truss.A > max_A] = max_A
+            self.truss._computeMass() # update mass
+            
+            self.TrussAnalysis() # recalculate forces
+            abs_max_tension = np.max([np.abs(np.min(self.getTension())), np.abs(np.max(self.getTension()))])       
+        
+        return self.truss.A
+    def homogenize_tensions(self, max_A, crit_tension):
+        """
+        Changes crossections such that tension is equal
+        returns: Array of crossections
+        """
+        abs_max_tension = np.max([np.abs(np.min(self.getTension())), np.abs(np.max(self.getTension()))])
+        if isinstance(self.truss.A, float) or isinstance(self.truss.A, int):
+            min_A = self.truss.A
+            self.truss.A = np.zeros(len(self.truss.bars))
+        else:
+            min_A = np.max(self.truss.A)
+        while abs_max_tension > crit_tension:
+            self.truss.A = np.abs(self.N) / (0.995*crit_tension) # increase crossections here
+            self.truss.A[self.truss.A < min_A] = min_A # keep crossections whithin allowed range
+            self.truss.A[self.truss.A > max_A] = max_A
+            self.TrussAnalysis() # recalculate forces
+            abs_max_tension = np.max([np.abs(np.min(self.getTension())), np.abs(np.max(self.getTension()))])
+            self.truss._computeMass() # update mass
+                
+        return self.truss.A
     def reset(self):
         """
         Removes all external forces including gravity and wind
@@ -247,52 +283,62 @@ class FEM:
         self.wind_dir = -1
         self.truss.reset()
         self.TrussAnalysis()
-
-    def map_value_to_color(self, value):
-        if value < 0 or value > 1:
-            raise Exception("The input value must lay between 0 and one!")
-
-        # define the color scale
-        color_scale = [
-            (0, (0, 255, 0)),  # green
-            (0.25, (255, 255, 0)),  # yellow
-            (0.5, (255, 165, 0)),  # orange
-            (0.75, (255, 0, 0)),  # red
-            (1, (128, 0, 128))  # purple
-        ]
-
+    def map_value_to_color(self, value, color_map):
+        """
+        Returns a hexadecimal color number as a string for a value between -1 and 1 based
+        on the given color map
+        """
+        if value < -1 or value > 1:
+            raise Exception("The input value must lay between -1 and 1!")
+        
         # find the appropriate color range for the value
-        for i in range(len(color_scale) - 1):
-            if value <= color_scale[i + 1][0]:
+        for i in range(len(color_map) - 1):
+            if value < color_map[i + 1][0]:
                 break
-
+        
         # interpolate between the colors
-        start_value, start_color = color_scale[i]
-        end_value, end_color = color_scale[i + 1]
+        start_value, start_color = color_map[i]
+        end_value, end_color = color_map[i + 1]
         ratio = (value - start_value) / (end_value - start_value)
         color = (
             int(start_color[0] + ratio * (end_color[0] - start_color[0])),
             int(start_color[1] + ratio * (end_color[1] - start_color[1])),
             int(start_color[2] + ratio * (end_color[2] - start_color[2]))
         )
-
+        
         # convert the color to HEX format
         hex_color = '#{:02x}{:02x}{:02x}'.format(*color)
-
+        
         return hex_color
-
-    def getColorMap(self):
-        tension = self.getTension()
-        min = np.min(tension)
-        max = np.max(tension)
+    
+    def getColorMap(self, values, min, max):
+        """
+        values : array-like
+        min :
+            minimum value to be assigned a color
+        max :
+            maximum value to be assigned a color
+        Returns:
+            * a color array that maps a color to each element in values
+            * the color map that was used
+        """
+        # define the color map
+        color_map = [
+            (-1, (128, 0, 128)),    # purple
+            (-2/3, (0, 0, 255)),   # blue
+            (-1/3, (0, 255, 255)), # cyan
+            (0, (0, 255, 0)),       # green
+            (1/3, (255, 255, 0)),  # yellow
+            (2/3, (255, 165, 0)),  # orange
+            (1, (200, 0, 64))       # dark red
+        ]
+        abs_max = np.max([np.abs(min), np.abs(max)]) # so that 0 is later mapped to zero
         color = []
-        for bar in self.truss.bars:
-            force = (tension[bar[0]] + tension[bar[1]]) / 2
-            value = (force - min) / (max - min)  # normalise value
-            color.append(self.map_value_to_color(value))
-        return color
-
-    def display(self, scale=1, external_forces=True, tension=False, wind=False):
+        for value in values:
+            color.append(self.map_value_to_color(value/abs_max, color_map)) # normalize value to fit interval [-1, 1]
+        return color, color_map
+    
+    def display(self, scale=1, external_forces=True, tension=False, wind=False, saveFig=None):
         """
         scale : float
             the scale in which the deformations should be displayed
@@ -302,67 +348,99 @@ class FEM:
             highlight the tension in each bar using a color map
         wind : bool
             highlight the surface at which wind is attacking
+        saveFig : string
+            name of the figure that should be saved
         """
         if tension and wind:
             print("It is not recommended to have both tension and wind exposed surfaces displayed!")
-
-        minTension = np.min(self.getTension()) / 1e6  # minimum tension [MPa]
-        maxTension = np.max(self.getTension()) / 1e6  # maximum tension [MPa]
-
+        tensions = self.getTension()
+        minTension = np.min(tensions)
+        maxTension = np.max(tensions)
+        if np.abs(minTension) >= 1e7 or np.abs(maxTension) >= 1e7:
+            minTension = minTension/1e6  
+            maxTension = maxTension/1e6
+            tensions = tensions/1e6
+            unit = 'MPa'
+        else: # both are < 1e7
+            minTension = minTension/1e3  
+            maxTension = maxTension/1e3
+            tensions = tensions/1e3
+            unit = 'KPa'
         # plot crane without deformations
         self.plot(self.truss.nodes, self.truss.bars, 'gray', '--', 1)
-
         # plot crane with deformations 
-        dnodes = self.U * scale + self.truss.nodes  # displacement of nodes
-
+        dnodes = self.U * scale + self.truss.nodes # displacement of nodes
         if not tension:
             self.plot(dnodes, self.truss.bars, 'red', '-', 2)
         else:
-            # visualize normal forces with colors 
-            colors = self.getColorMap()
+            # visualize tensions with colors 
+            colors, color_map = self.getColorMap(tensions, minTension, maxTension)
             self.plot(dnodes, self.truss.bars, colors, '-', 2)
+            
             # legend
-            color_scale = [
-                (0, 255, 0),  # green
-                (255, 255, 0),  # yellow
-                (255, 165, 0),  # orange
-                (255, 0, 0),  # red
-                (128, 0, 128)]  # purple
-
-            lines = [plt.Line2D([0], [0], color='#{:02x}{:02x}{:02x}'.format(*color_scale[0]), lw=2),
-                     plt.Line2D([0], [0], color='#{:02x}{:02x}{:02x}'.format(*color_scale[1]), lw=2),
-                     plt.Line2D([0], [0], color='#{:02x}{:02x}{:02x}'.format(*color_scale[2]), lw=2),
-                     plt.Line2D([0], [0], color='#{:02x}{:02x}{:02x}'.format(*color_scale[3]), lw=2),
-                     plt.Line2D([0], [0], color='#{:02x}{:02x}{:02x}'.format(*color_scale[4]), lw=2)]
-
-            labels = [str(int(minTension)) + " MPa",
-                      str(int(minTension + 0.25 * (maxTension - minTension))) + " MPa",
-                      str(int(minTension + 0.5 * (maxTension - minTension))) + " MPa",
-                      str(int(minTension + 0.75 * (maxTension - minTension))) + " MPa",
-                      str(int(maxTension)) + " MPa"]
-
+            color_map = np.array(color_map)
+            abs_max = np.max([np.abs(minTension), np.abs(maxTension)])
+            legend_values = []
+            legend_values.append(minTension) # start at minimum tension
+            for value in color_map[:, 0]:
+                # it could be that there are colors defined for nonexisting tensions
+                # we don't want those
+                if value*abs_max <= minTension:
+                    continue
+                legend_values.append(value*abs_max) # fill up till maximum is reached
+            lines = []
+            legend_colors, _ = self.getColorMap(legend_values, minTension, maxTension)
+            for color in legend_colors:
+                lines.append(plt.Line2D([0], [0], color=color, lw=2))
+            lines.reverse()
+            
+            labels = []
+            for value in legend_values:
+                labels.append(str(int(value))+" "+unit)
+            labels.reverse()
             plt.legend(lines, labels, title="Spannungen")
-
         if external_forces:
             for i in self.truss.force_points:
                 self.plotPoint(dnodes[i])
-
         if wind:
             if not self.wind:
-                pass
+                print("There is no wind.")
+            else:
+                if self.wind_dir == 0:
+                    self.plot(dnodes, self.truss.bars[self.truss.x_positive_side], 'lightskyblue', '-', 2, 'wind exposed area')
+                elif self.wind_dir == 1:
+                    self.plot(dnodes, self.truss.bars[self.truss.x_negative_side], 'lightskyblue', '-', 2, 'wind exposed area')
+                elif self.wind_dir == 2:
+                    self.plot(dnodes, self.truss.bars[self.truss.y_positive_side], 'lightskyblue', '-', 2, 'wind exposed area')
+                elif self.wind_dir == 3:
+                    self.plot(dnodes, self.truss.bars[self.truss.y_negative_side], 'lightskyblue', '-', 2, 'wind exposed area')
 
         plt.suptitle(self.truss)
-        plt.title("spannung: [" + str(int(minTension)) + ", " + str(int(maxTension)) + "] MPa", fontsize=10)
-        # save plot
-        # plt.savefig('figures/fig1', dpi=600)
+        plt.title("Höhe: "+str(self.truss.height)+" m\n"
+                + "Länge: "+str(self.truss.length)+" m\n"
+                + "Segmentlänge: "+str(np.round(self.truss.ls, 2))+" m\n"
+                + "Masse: "+str(int(np.sum(self.truss.mass)))+" kg", fontsize=8, y = 0.95, loc="right")
+        
+        if saveFig is not None:
+            plt.savefig('figures/'+str(saveFig), dpi=600)
+
         plt.show()
 
     def plot(self, nodes, bars, color, lt, lw, lg=None):
+        """
+        nodes : number array
+        bars : number array
+        color : string or array of string
+        lt : string
+            line style
+        lw : number
+            linewidth
+        """
         if isinstance(color, str):
             color = [color] * len(bars)
         plt.subplot(projection='3d')
         plt.gca().set_aspect('equal')
-        # plt.gca(projection='3d')
+        plt.gca().view_init(elev=19, azim=-57, roll=0)
         for i in range(len(bars)):
             # each start and end coordinate
             xi, xf = nodes[bars[i, 0], 0], nodes[bars[i, 1], 0]
