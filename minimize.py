f71e516b1adc68713a55a922c7072438580b5d40
Shun Zhang <shun.zhang06@outlook.com>
2023-07-06 16:25:12 +0200

    revert

--- minimize.py
+++ minimize.py
@@ -7,10 +7,9 @@ import os
 
 E = 210e9  # E-module [Pa]
 rho = 7850  # density [kg/m^3]
-price = 1  # Price [1 €/kg]
+price = 1
 
-
-def calculateCov(range_min: np.ndarray, range_max: np.ndarray):
+def calculateCov(range_min:np.ndarray,range_max: np.ndarray):
     """
     This function returns the covariance matrix
     @param range_min:
@@ -18,10 +17,9 @@ def calculateCov(range_min: np.ndarray, range_max: np.ndarray):
     @return: covariance matrix
     """
     # Calculate variances for each dimension
-    variances = (range_max - range_min) / 4
+    variances = ((range_max - range_min)) /4
     cov = np.diag(variances)
     return cov
-
 
 def eigDecomposition(cov):
     """
@@ -37,7 +35,7 @@ def eigDecomposition(cov):
     return A
 
 
-def cem(crane, iterations, batch_size, waning_time=1, additional_std=0.35, fraction=0.15):
+def cem(crane,iterations,batch_size,waning_time= 1,additional_std=0.35,fraction=0.15):
     """
     This function runs cross-entropy method to search the best combination of parameter set that has minimal cost
     @param iterations: total iterations that the function runs
@@ -47,96 +45,94 @@ def cem(crane, iterations, batch_size, waning_time=1, additional_std=0.35, fract
     @param fraction: percentage of the top tier
     @return:
     """
-    # set mean rewards
+    #set mean rewards
     mean_rewards = []
     mean_rewards.append(0)
     # initialize mean and standard deviation
-    theta_mean = np.array([10, 10, 1.25, 0.0325])
-    # length and height are set around 10
+    theta_mean = np.array([10,10,1.25,0.0325])
+    #length and height are set around 10
     A = eigDecomposition(calculateCov(np.array([9.9, 9.9, 0.5, 0.0025]), np.array([10.1, 10.1, 2, 0.0625])))
     cons_cov = eigDecomposition(calculateCov(np.array([9.9, 9.9, 0.5, 0.0025]), np.array([10.1, 10.1, 2, 0.0625])))
-    mean_x = []
+    mean_x=[]
     for iteration in range(iterations):
         # initialize batch
-        theta_set = []
-        rewards = []
-        print("-----------------------iteration: ", iteration, "--------------------------")
+        theta_set=[]
+        rewards=[]
+        print("-----------------------iteration: ",iteration,"--------------------------")
         np.random.seed(None)
         for batch in range(batch_size):
             # for each batch
             # X = AZ + u
             # see multi-variant distribution
             # theta=(theta_cov + (np.identity(5)*max(1 - iteration / waning_time, 0) * additional_std**2)) @ np.random.randn(5) +theta_mean
-            x = (A + (cons_cov * max(1 - iteration / waning_time, 0) * additional_std ** 2)) @ np.random.randn(
-                4) + theta_mean
+            x = (A + (cons_cov * max(1 - iteration / waning_time, 0) * additional_std ** 2)) @ np.random.randn(4) + theta_mean
             # normal distribution generate some samples with negative value, need absolute value
-            x = np.abs(x)
+            x=np.abs(x)
             # initialize crane object
             # manually selects the samples within certain range
             x[0] = np.clip(x[0], 9.9, 10)
             x[1] = np.clip(x[1], 9.9, 10)
-            x[2] = np.clip(x[2], 0.5, 2)
-            x[3] = np.clip(x[3], 0.0025, 0.0625)
-            myCrane = crane(x[0], x[1], x[2], x[3], rho, E, False)
+            x[2] = np.clip(x[2],0.5,2)
+            x[3] = np.clip(x[3],0.0025,0.0625)
+            myCrane = crane( x[0], x[1], x[2], x[3],  rho, E, False)
             for i in range(-1, -5, -1):
                 myCrane.addExternalForce(i, 0, 0, -500e3 / 4)
             fem = FEM(myCrane, False)
             # calculate tension and cost
             t = np.abs(np.max(fem.getTension()))
-            cost = -np.sum(myCrane.mass) * price
+            cost = -np.sum(myCrane.mass)*price
             # check condition
-            if t >= 0.2e9 or not fem.check_bending_force():
+            if t >=0.2e9 or not fem.check_bending_force():
                 continue
             # store sample
             theta_set.append(x)
             rewards.append(cost)
         # process that selects elite sets
         index = np.argsort(np.array(rewards))
-        # reverse sequence, so we have [987654321]
+        #reverse sequence, so we have [987654321]
         index = index[::-1]
         elite = []
         # number of candidates
         num_top = int(fraction * len(theta_set))
         # append candidates
-        for i in range(num_top):
+        for i in range (num_top):
             elite.append(theta_set[index[i]])
         # append corresponding parameter sets of candidates
-        matrix = np.stack(elite, axis=0).T
+        matrix =np.stack(elite,axis=0).T
         # calculate mean value of all parameter in this elite sets
         for i in range(4):
-            theta_mean[i] = np.mean(matrix[i, :])
+            theta_mean[i] = np.mean(matrix[i,:])
         # deep copy into returned lists
         mean_x.append(np.copy(theta_mean))
         # generate new covariance for the next iteration
         # cov = AA.T, using eigenvalue decomposition
         A = eigDecomposition(np.cov(matrix))
-        diff = np.abs(mean_rewards[-1] - np.mean(rewards))
-        print("---------------diff: ", diff, "---------------------")
-        if (diff < 10):
+        diff = np.abs(mean_rewards[-1]-np.mean(rewards))
+        print("---------------diff: ",diff,"---------------------")
+        if(diff<10):
             break
         mean_rewards.append(np.mean(rewards))
-    return [mean_rewards[-1], mean_x[-1]]
+    return [mean_rewards[-1] ,mean_x[-1]]
 
 
 def sample(crane, mean, A, cons_cov, iteration, waning_time, additional_std):
     np.random.seed(None)
     x = (A + (cons_cov * max(1 - iteration / waning_time, 0) * additional_std ** 2)) @ np.random.randn(2) + mean
     x[1] = np.clip(x[1], 0.5, 1.414)
-    x[0] = np.clip(np.abs(x[0]), 0, 500e3)
+    x[0] = np.clip(np.abs(x[0]), 0, 10*500e3)
     myCrane = crane(10, 10, x[1], 0.0025, rho, E, False)
     for i in myCrane.tip_nodes:
-        myCrane.addExternalForce(i, 0, 0, -500e3 / 4)
+        myCrane.addExternalForce(i, 0, 0, -500e3/len(myCrane.tip_nodes))
     # counterweight
     for i in myCrane.counterweight_nodes:
         myCrane.addExternalForce(i, 0, 0, -x[0] / len(myCrane.counterweight_nodes))
     fem = FEM(myCrane, own_weight=True)
-    fem.homogenize_tensions(625e-4, 200e6)
+    fem.optimize_crossections(625e-4, 0.9*200e6)
     cost = -np.sum(fem.truss.mass)
-    print("sample: ", x, "after： ", cost)
+    print("sample: ",x,"after： ",cost)
     return [x, cost]
 
-
-def cem_slcw(crane, iterations, batch_size, waning_time=10, additional_std=0.5, fraction=0.15):
+def cem_slcw(crane,iterations,batch_size,waning_time= 1,additional_std=0.3,fraction=0.15):
     """
     This function runs cross-entropy method to search the best combination of parameter set that has minimal cost
     @param crane: crane version
@@ -147,23 +143,21 @@ def cem_slcw(crane, iterations, batch_size, waning_time=10, additional_std=0.5, 
     @param fraction: percentage of the top tier
     @return:
     """
-    # set mean rewards
+    #set mean rewards
     mean_rewards = []
     mean_rewards.append(0)
     # initialize mean and standard deviation
-    theta_mean = np.array([250e3, 0.957])
-    # length and height are set around 10
-    A = calculateCov(np.array([0, 0.5]), np.array([500e3, 1.414]))
-    mean_x = []
+    theta_mean = np.array([2500e3,0.957])
+    #length and height are set around 10
+    A = calculateCov(np.array([0, 0.5]), np.array([10*500e3,1.414]))
+    mean_x=[]
     for iteration in range(iterations):
         # initialize batch
-        results = []
-        print("-----------------------iteration: ", iteration, "--------------------------")
-
+        results=[]
+        print("-----------------------iteration: ",iteration,"--------------------------")
         def transfer(result):
             results.append(result)
             return
-
         """
         Pool(enter your number of physical cpu)
         """
@@ -172,48 +166,58 @@ def cem_slcw(crane, iterations, batch_size, waning_time=10, additional_std=0.5, 
             """
             try to parallelize the loop
             """
-            p.apply_async(sample, args=(crane, theta_mean, A, A, iteration, waning_time, additional_std),
-                          callback=transfer)
+            p.apply_async(sample, args=(crane,theta_mean,A,A,iteration,waning_time,additional_std),callback=transfer)
+
         # process that selects elite sets
         p.close()
         p.join()
         rewards = [results[i][1] for i in range(len(results))]
         theta_set = [results[i][0] for i in range(len(results))]
         index = np.argsort(np.array(rewards))
-        # reverse sequence, so we have [987654321]
+        #reverse sequence, so we have [987654321]
         index = index[::-1]
         elite = []
         # number of candidates
         num_top = int(fraction * len(theta_set))
         # append candidates
-        for i in range(num_top):
+        for i in range (num_top):
             elite.append(theta_set[index[i]])
         # append corresponding parameter sets of candidates
-        matrix = np.stack(elite, axis=0).T
+        matrix =np.stack(elite,axis=0).T
         # calculate mean value of all parameter in this elite sets
         for i in range(2):
-            theta_mean[i] = np.mean(matrix[i, :])
+            theta_mean[i] = np.mean(matrix[i,:])
         # deep copy into returned lists
         mean_x.append(np.copy(theta_mean))
         # generate new covariance for the next iteration
         # cov = AA.T, using eigenvalue decomposition
         A = eigDecomposition(np.cov(matrix))
-        diff = np.abs(mean_rewards[-1] - np.mean(rewards))
-        print("---------------diff: ", diff, "---------------------")
-        if (diff < 10):
+        diff = np.abs(mean_rewards[-1]-np.mean(rewards))
+        print("---------------diff: ",diff,"---------------------")
+        if(diff<5):
             break
         mean_rewards.append(np.mean(rewards))
-    return mean_rewards, mean_x
+    return mean_rewards ,mean_x
+
+
 
 
 if __name__ == "__main__":
     """
     Print result for one optimization
     """
-    rewards, x = cem_slcw(crane.crane_2_1, 100, 500, waning_time=1, fraction=0.1)
-    print("theta: " + str(x[-1]))
+    rewards, x = cem_slcw(crane.crane_2_1, 100, 1000,waning_time=3,additional_std=1,fraction=0.25)
+    print("theta: "+ str(x[-1]))
     print("reward: " + str(rewards[-1]))
 
+    """
+    Print result in chronological order
+    """
+    plt.figure(constrained_layout=True)
+    plt.plot(rewards[1:])
+    plt.xlabel("Iteration")
+    plt.ylabel("Cost")
+    plt.show()
     """
     multiple times optimization
     """
